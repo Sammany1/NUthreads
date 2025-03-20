f9c6feaaa1ba7d238273d6e4c51519e0da4dcd6d
Sammany1 <ialsammany@gmail.com>
2025-03-20 14:18:12 +0200

    changed file naming from DependencyInjection to ServiceCollectionExtensions

--- NUthreads.Application/ServiceCollectionExtensions.cs
+++ NUthreads.Application/ServiceCollectionExtensions.cs
@@ -1,15 +1,14 @@
-using System.Globalization;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace NUthreads.Application;
 
-public static class DependencyInjection
+public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddApplication(this IServiceCollection service)
     {
         service.AddMediatR(configuration =>
             configuration.RegisterServicesFromAssembly(
-                typeof(DependencyInjection).Assembly));
+                typeof(ServiceCollectionExtensions).Assembly));
         
         return service;
     }
